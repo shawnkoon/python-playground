dc209d2a2c3243edd22d3d7da75ba2d199947686
Shawn Choi <shawn.choi@us.imshealth.com>
2018-10-03 20:59:51 -0700

    Add in memory store items api

--- flask_rest/ReadMe.md
+++ flask_rest/ReadMe.md
@@ -1,3 +1,9 @@
 # Flask REST API Application.
 
 Build REST API using Flask-Restful.
+
+
+## How to run.
+
+- `make run`
+- `python app.py`
--- flask_rest/app.py
+++ flask_rest/app.py
@@ -1,15 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
     def get(self, name):
-        return { 'student': name }
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return { 'item': item }, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return { 'message': 'Item with name \'{}\' already exists.'.format(name)}, 400
+
+        new_item = request.get_json()
+        item = { 'name': name, 'price': new_item['price'] }
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        if item: # update
+            item.update(data)
+        else: # create
+            item = { 'name': name, 'price': data['price'] }
+            items.append(item)
+
+        return item
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return { 'message': 'Item successfully removed.'}
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return { 'items': items }
 
-app.run(port=5050)
+
+api.add_resource(ItemList, '/items')
+api.add_resource(Item, '/items/<string:name>')
+
+app.run(port=5050, debug=True)
